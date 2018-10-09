b7c4c3145db5fc00009b1898124d29cd3e177f43
yang <735600970@qq.com>
2018-10-09 16:00:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 # `/Users/yang/anaconda3/envs/py36web` 这部分修改本机对应的环境
 export PATH="/home/yang/Applications/anaconda3/envs/py36web/bin:${PATH}"
-exec gunicorn -b 127.0.0.1:30000 -w 3 start:app
+exec gunicorn -k gevent --worker-connections 10 -b 127.0.0.1:30000 -w 3 start:app
